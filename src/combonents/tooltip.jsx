45bc5ebff054b9182ec95362c9e42575bf834d35
asim16111980 <abomido2892016@gmail.com>
2025-01-18 01:15:17 +0200

    Complete computeTooltipPosition function

--- src/combonents/tooltip.jsx
+++ src/combonents/tooltip.jsx
@@ -4,12 +4,28 @@ const Tooltip = forwardRef((props, ref) => {
   return (
     <div
       ref={ref}
-      className={"opacity-0 invisisble w-max flex justify-center items-center group-hover:opacity-100 group-hover:visible transition-all duration-300  absolute p-2 bg-sky-950 rounded-lg -top-full left-[50%] -translate-x-1/2 " + (props.position=="top" ? '-top-full':props.position=="right" ? 'right:0':'') }
+      className={`opacity-0 invisisble w-max h-max flex items-center group-hover:opacity-100 group-hover:visible transition-all duration-300 absolute bg-transparent z-10 ${
+        props.position == "top"
+          ? "flex-col bottom-[calc(100%-8px)] left-[50%] -translate-x-1/2"
+          : props.position == "right"
+          ? "flex-row-reverse left-[calc(100%-8px)] top-[50%] -translate-y-1/2"
+          : props.position == "bottom"
+          ? "flex-col-reverse top-[calc(100%-8px)] left-[50%] -translate-x-1/2"
+          : "flex-row right-[calc(100%-8px)] top-[50%] -translate-y-1/2"
+      }`}
     >
-      <span className="text-white text-xs">{props.text}</span>
+      <span className="rounded-lg p-2 bg-sky-600 text-white text-xs">{props.text}</span>
       <span
-        className="absolute top-8 left-9 border-8 border-transparent
-       border-t-sky-950"
+        className={`border-8 border-transparent
+        ${
+          props.position == "top"
+            ? "border-t-sky-600"
+            : props.position == "right"
+            ? "border-r-sky-600"
+            : props.position == "bottom"
+            ? "border-b-sky-600"
+            : "border-l-sky-600"
+        }`}
       ></span>
     </div>
   );
